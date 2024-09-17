bcc02e8aaafa3eb03546e9123add9d4ab3fed879
Kiyohiro Kambayashi <k.kambayashi@westernsydney.edu.au>
2024-09-17 23:07:31 +1000

    Create repo to test cicd

---
+++ function/s3.ts
@@ -0,0 +1,14 @@
+import { Website } from "./type";
+import { S3 } from "aws-sdk";
+
+// Get website data from S3
+export async function getWebsitesFromS3(
+    bucket: string,
+    key: string
+  ): Promise<Website[]> {
+    const s3 = new S3();
+    const params = { Bucket: bucket, Key: key };
+    const data = await s3.getObject(params).promise();
+
+    return JSON.parse(data.Body?.toString("utf-8") || "[]") as Website[];
+  }
